fc5cb72cefcdca659eb32a59fc55178a8264f5b8
Ksihris <xihris@gmail.com>
2017-05-08 18:07:07 +0300

    edit validates and views

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,5 +18,7 @@ class UsersController < ApplicationController
     @questions = [
       Question.new(text: 'Как дела?', created_at: Date.parse('27.03.2016'))
       ]
+
+    @new_question = Question.new
   end
 end
